26591ba60aebc72b81da73ea834a6caa9cbe75e4
Douglas Silva <douglaseusilva@gmail.com>
2021-10-04 18:00:27 -0300

    chore: updating env vars

--- knexfile.ts
+++ knexfile.ts
@@ -8,8 +8,8 @@ module.exports = {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT as string),
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || 'cdu'
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME || 'test'
   },
   pool: {
     min: 2,
--- src/main/config/config.ts
+++ src/main/config/config.ts
@@ -6,8 +6,8 @@ export const env = {
     client: process.env.DB_CLIENT || 'mysql',
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT) || 3306,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     name: process.env.DB_NAME,
     migrations_dir: process.env.DB_MIGRATIONS_DIR || 'src/main/config/database/migrations',
     seeds_dir: process.env.DB_SEEDS_DIR || 'src/main/config/database/seeds'
