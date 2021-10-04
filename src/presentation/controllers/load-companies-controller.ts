fc52d265b0fbb1ab69b83b408d4ff324ee596cfa
Douglas Silva <douglaseusilva@gmail.com>
2021-10-04 15:28:20 -0300

    feat: load companies controller

---
+++ src/presentation/controllers/load-companies-controller.ts
@@ -0,0 +1,15 @@
+import { getDatabaseConnection } from '@/infra/repositories/database-connection'
+import { Controller, HttpResponse } from '../contracts'
+import { ok, serverError } from '../helpers/http-helper'
+
+export class LoadCompaniesController implements Controller {
+  async handle (): Promise<HttpResponse> {
+    try {
+      const connection = getDatabaseConnection()
+      const companies = await connection('company').select('*')
+      return ok(companies)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
