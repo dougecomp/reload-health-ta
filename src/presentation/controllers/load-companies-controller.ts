cc289613b1ecf0d5cf45193a2d94cbe9964c1216
Douglas Silva <douglaseusilva@gmail.com>
2021-10-05 17:04:31 -0300

    chore: removing const connectino

--- src/presentation/controllers/load-companies-controller.ts
+++ src/presentation/controllers/load-companies-controller.ts
@@ -5,8 +5,7 @@ import { ok, serverError } from '../helpers/http-helper'
 export class LoadCompaniesController implements Controller {
   async handle (): Promise<HttpResponse> {
     try {
-      const connection = getDatabaseConnection()
-      const companies = await connection('companies').select('*')
+      const companies = await getDatabaseConnection()('companies').select('*')
       return ok(companies)
     } catch (error) {
       return serverError(error)
