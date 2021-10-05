0853ed3816250ddb29468168b6960cda318690d3
Douglas Silva <douglaseusilva@gmail.com>
2021-10-05 15:19:32 -0300

    feat: two versions to seed companies.
    Seed reading file with readFileSync.
    Seed streaming file with readStream.

--- src/main/config/database/seeds/000001_populate_companies.ts
+++ src/main/config/database/seeds/000001_populate_companies.ts
@@ -1,15 +1,91 @@
+import fs from 'fs'
+import { Writable, pipeline } from 'stream'
+import { promisify } from 'util'
+
 import { Knex } from 'knex'
-import data from '../data.json'
+import StreamArray from 'stream-json/streamers/StreamArray'
 
-export async function seed (knex: Knex): Promise<void> {
+const promisedPipeline = promisify(pipeline)
+
+/**
+ * ! 479 lines could not be inserted with duplicated id
+ * @see seed_error_log.txt on root folder to see
+ *
+ */
+
+// * Seed version with streaming loading each company on demand in memory and inserting on database
+// * Average of 100 seconds in 3 consecutive executions to process 10.000 companies, 36.746 contributors and 36.746 desktops
+export async function seed (knex: Knex) {
   const transaction = await knex.transaction()
+  await transaction('companies').del()
 
-  try {
-    await knex('companies').del()
+  const readStream = fs.createReadStream('data.json')
+  const arrayJsonParserStream = StreamArray.withParser()
+  const processingStream = new Writable({
+    write: async ({ key, value: company }, encoding, cb) => {
+      try {
+        await transaction('companies').insert({
+          id: company.id,
+          business_name: company.business_name,
+          suffix: company.suffix,
+          industry: company.industry,
+          catch_phrase: company.catch_phrase,
+          bs_company_statement: company.bs_company_statement,
+          logo: company.logo,
+          type: company.type,
+          phone_number: company.phone_number,
+          latitude: company.latitude,
+          longitude: company.longitude
+        })
+        for (const contributor of company.contributors) {
+          await transaction('contributors').insert({
+            firstName: contributor.firstName,
+            lastName: contributor.lastName,
+            title: contributor.title,
+            jobTitle: contributor.jobTitle,
+            age: contributor.age,
+            id_company: company.id
+          })
+        }
+        for (const desktop of company.desktops) {
+          await transaction('desktops').insert({
+            platform: desktop.platform,
+            type: desktop.type,
+            os: desktop.os,
+            ip: desktop.ip,
+            id_company: company.id
+          })
+        }
+      } catch (error) {
+        console.log(`Company ${company.id} could not be imported due to following error: ${error}`)
+      } finally {
+        cb()
+      }
+    },
+    objectMode: true
+  })
 
-    const companies = data as any[]
-    for (const company of companies) {
-      await knex('companies').insert({
+  await promisedPipeline(
+    readStream,
+    arrayJsonParserStream,
+    processingStream
+  )
+
+  await transaction.commit()
+}
+
+// * Traditional seed version loading entire file on memory and inserting on database
+// * Average of 91 in 3 consecutive executions seconds to process 10.000 companies, 36.746 contributors and 36.746 desktops
+/* export async function seed (knex: Knex): Promise<void> {
+  const transaction = await knex.transaction()
+  await transaction('companies').del()
+
+  const data = fs.readFileSync('data.json')
+  const companies = JSON.parse(data.toString())
+
+  for (const company of companies) {
+    try {
+      await transaction('companies').insert({
         id: company.id,
         business_name: company.business_name,
         suffix: company.suffix,
@@ -23,7 +99,7 @@ export async function seed (knex: Knex): Promise<void> {
         longitude: company.longitude
       })
       for (const contributor of company.contributors) {
-        await knex('contributors').insert({
+        await transaction('contributors').insert({
           firstName: contributor.firstName,
           lastName: contributor.lastName,
           title: contributor.title,
@@ -33,7 +109,7 @@ export async function seed (knex: Knex): Promise<void> {
         })
       }
       for (const desktop of company.desktops) {
-        await knex('desktops').insert({
+        await transaction('desktops').insert({
           platform: desktop.platform,
           type: desktop.type,
           os: desktop.os,
@@ -41,9 +117,10 @@ export async function seed (knex: Knex): Promise<void> {
           id_company: company.id
         })
       }
+    } catch (error) {
+      console.log(`Company ${company.id} could not be imported due to following error: ${error}`)
+      continue
     }
-    await transaction.commit()
-  } catch (error) {
-    await transaction.rollback()
   }
-};
+  await transaction.commit()
+} */
