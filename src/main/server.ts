07a2d707f2e785ebec2c3686afee6e39afbf82eb
Douglas Silva <douglaseusilva@gmail.com>
2021-10-05 18:01:03 -0300

    chore: using IIFE on server start to ensure redis connection

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,23 +1,27 @@
 import { env } from './config/config'
 import { makeApp } from './app'
 import { setDatabaseConnection } from '@/infra/repositories'
-import { setRedisConnection } from '@/infra/redis/redis-connection'
+import { getRedisConnection, setRedisConnection } from '@/infra/redis/redis-connection'
 
-const app = makeApp()
+(async () => {
+  const app = makeApp()
 
-setDatabaseConnection({
-  client: env.database.client,
-  host: env.database.host,
-  port: env.database.port,
-  user: env.database.user,
-  password: env.database.password,
-  database: env.database.name
-})
+  setDatabaseConnection({
+    client: env.database.client,
+    host: env.database.host,
+    port: env.database.port,
+    user: env.database.user,
+    password: env.database.password,
+    database: env.database.name
+  })
 
-setRedisConnection({
-  url: env.redis.host
-})
+  setRedisConnection({
+    url: env.redis.host
+  })
 
-app.listen(env.port, function () {
-  console.log(`Server listening at port ${env.port}`)
-})
+  await getRedisConnection().connect()
+
+  app.listen(env.port, function () {
+    console.log(`Server listening at port ${env.port}`)
+  })
+})()
