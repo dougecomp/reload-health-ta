0bea3d855884be8a1b513880b8a3de8f4c0caaf3
Douglas Silva <douglaseusilva@gmail.com>
2021-10-05 23:43:45 -0300

    feat: search companies by term route

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,16 +1,11 @@
-import { Next, Request, Response, Server } from 'restify'
+import { Server } from 'restify'
 
 import { adaptRestifyRoute } from '@/main/adapters'
-import { LoadCompaniesController, LoadContributorsByCompanyController, LoadDesktopsByCompanyController, LoadDesktopsController } from '@/presentation/controllers'
-
-function dummyResponse (req: Request, res: Response, next: Next) {
-  res.send('hello')
-  next()
-}
+import { LoadCompaniesController, LoadContributorsByCompanyController, LoadDesktopsByCompanyController, LoadDesktopsController, SearchCompaniesController } from '@/presentation/controllers'
 
 export function makeRoutes (server: Server) {
   server.get('/companies', adaptRestifyRoute(new LoadCompaniesController()))
-  server.post('/companies/search', dummyResponse)
+  server.post('/companies/search', adaptRestifyRoute(new SearchCompaniesController()))
   server.get('/companies/desktops', adaptRestifyRoute(new LoadDesktopsController()))
   server.get('/companies/:companyId/desktops', adaptRestifyRoute(new LoadDesktopsByCompanyController()))
   server.get('/companies/:companyId/contributors', adaptRestifyRoute(new LoadContributorsByCompanyController()))
---
+++ src/presentation/controllers/search-companies-controller.ts
@@ -0,0 +1,58 @@
+import { getDatabaseConnection } from '@/infra/repositories'
+import { Controller, HttpResponse } from '../contracts'
+import { ok, serverError } from '../helpers/http-helper'
+
+export class SearchCompaniesController implements Controller {
+  async handle (request: any): Promise<HttpResponse> {
+    try {
+      const { term } = request
+      const companiesColumnsToSearch = [
+        'id',
+        'business_name',
+        'suffix',
+        'industry',
+        'catch_phrase',
+        'bs_company_statement',
+        'logo',
+        'type',
+        'phone_number',
+        'latitude',
+        'longitude'
+      ]
+      const contributorsColumnsToSearch = [
+        'id',
+        'firstName',
+        'lastName',
+        'title',
+        'jobTitle',
+        'age'
+      ]
+      const desktopsColumnsToSearch = [
+        'id',
+        'platform',
+        'type',
+        'os',
+        'ip',
+        'id_company'
+      ]
+      const queryBuilder = getDatabaseConnection()('companies')
+        .select('companies.*')
+        .join('contributors', 'companies.id', 'contributors.id_company')
+        .join('desktops', 'companies.id', 'desktops.id_company')
+        .distinct()
+      companiesColumnsToSearch.forEach(column => {
+        queryBuilder.orWhere(`companies.${column}`, 'like', `%${term}%`)
+      })
+      contributorsColumnsToSearch.forEach(column => {
+        queryBuilder.orWhere(`contributors.${column}`, 'like', `%${term}%`)
+      })
+      desktopsColumnsToSearch.forEach(column => {
+        queryBuilder.orWhere(`desktops.${column}`, 'like', `%${term}%`)
+      })
+      const companies = await queryBuilder
+      return ok(companies)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
