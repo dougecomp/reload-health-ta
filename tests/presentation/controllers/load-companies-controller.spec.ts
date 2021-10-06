ad05b3433d590767395875c216ae7fad9be0cce6
Douglas Silva <douglaseusilva@gmail.com>
2021-10-05 21:14:39 -0300

    tests: using redis-mock

--- tests/presentation/controllers/load-companies-controller.spec.ts
+++ tests/presentation/controllers/load-companies-controller.spec.ts
@@ -5,24 +5,29 @@ import chaiAsPromised from 'chai-as-promised'
 import { createSandbox, SinonSandbox } from 'sinon'
 import { Knex } from 'knex'
 import mockDb from 'mock-knex'
+import { RedisClientType } from 'redis/dist/lib/client'
+import redisMock from 'redis-mock'
 
 import { ok } from '@/presentation/helpers/http-helper'
 import { LoadCompaniesController } from '@/presentation/controllers'
 import { getDatabaseConnection, setDatabaseConnection } from '@/infra/repositories'
+import * as redisLib from '@/infra/redis/redis-connection'
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
 const expect = chai.expect
 
 describe('Load Companies Controller', () => {
-  let tracker: mockDb.Tracker
+  let redisClient: RedisClientType
+  let databaseTracker: mockDb.Tracker
   let databaseConnection: Knex<any, unknown[]>
   let sut: LoadCompaniesController
   let sandbox: SinonSandbox
 
   before(() => {
+    redisClient = redisMock.createClient()
     setDatabaseConnection({
-      client: '',
+      client: 'mysql',
       database: '',
       host: '',
       password: '',
@@ -31,12 +36,13 @@ describe('Load Companies Controller', () => {
     })
     databaseConnection = getDatabaseConnection()
     mockDb.mock(databaseConnection)
-    tracker = mockDb.getTracker()
-    tracker.install()
+    databaseTracker = mockDb.getTracker()
+    databaseTracker.install()
     sandbox = createSandbox()
   })
 
   beforeEach(() => {
+    sandbox.stub(redisLib, 'getRedisConnection').returns(redisClient)
     sut = new LoadCompaniesController()
   })
 
@@ -45,18 +51,15 @@ describe('Load Companies Controller', () => {
   })
 
   after(() => {
-    tracker.uninstall()
+    databaseTracker.uninstall()
     mockDb.unmock(databaseConnection)
   })
 
-  it('should return return 200 success', async () => {
-    const tracker = mockDb.getTracker()
-    tracker.install()
-    tracker.on('query', (query) => {
+  it('should return 200 on success', async () => {
+    databaseTracker.on('query', (query) => {
       query.response([])
     })
     const response = await sut.handle()
     expect(response).to.be.deep.equals(ok([]))
-    tracker.uninstall()
   })
 })
